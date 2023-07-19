af3747f8b115d0086e26ce3ff78ad05a01ee205e
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-07-20 01:09:37 +0900

    [fix]firebase連携

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:runpush/service/api_service.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'model/app_config.dart';
+
+void main() => run(
+  AppConfig(
+    androidPackageName: 'com.example.runpush',
+    iOSBundleId: 'com.example.runpush',
+    firebaseOptions: DefaultFirebaseOptions.currentPlatform,
+  ),
+);
+
+Future<void> run(AppConfig config) async {
+  Get.lazyPut(ApiService.new);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: config.firebaseOptions);
   runApp(const MyApp());
 }
 
@@ -43,47 +60,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
--- lib/model/app_config.dart
+++ lib/model/app_config.dart
@@ -4,18 +4,12 @@ import 'app_flavor.dart';
 
 class AppConfig {
   AppConfig({
-    required this.flavor,
     required this.androidPackageName,
     required this.iOSBundleId,
     required this.firebaseOptions,
-    required this.domain,
-    required this.dynamicLinkDomain,
   });
 
-  final AppFlavor flavor;
   final String androidPackageName;
   final String iOSBundleId;
   final FirebaseOptions firebaseOptions;
-  final String domain;
-  final String dynamicLinkDomain;
 }
