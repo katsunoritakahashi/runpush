ddfa57b09f3354f5b963cd73b9055fd23727901c
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-07-22 16:17:43 +0900

    [fix]トップ画面実装

--- lib/ui/view/top_view.dart
+++ lib/ui/view/top_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:runpush/ui/controller/setting_controller.dart';
 import '../common/app_color.dart';
-import '../widget/primary_button.dart';
+import '../common/ui_helper.dart';
 
 class TopView extends StatelessWidget {
   const TopView({super.key});
@@ -19,11 +19,54 @@ class TopView extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            PrimaryButton(label: '設定する', onPressed: () => (){},)
+          children: const [
+            Text(
+              'PUSH(応援)して欲しい\nキャラクターを選んでねっ！😋',
+              style: TextStyle(color: primaryColor, fontWeight: FontWeight.bold, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              '選んだキャラクターから応援メッセージが届くよ！',
+              style: TextStyle(color: gray3, fontSize: 14),
+              textAlign: TextAlign.center,
+            ),
+            verticalSpaceSmall,
+            _CharacterCard(imageUrl: 'assets/images/runtekun.png', name: 'カエルとキツネのキメラ'),
+            verticalSpaceSmall,
+            _CharacterCard(imageUrl: 'assets/images/hisaju_san.png', name: '校長'),
+            verticalSpaceSmall,
+            _CharacterCard(imageUrl: 'assets/images/pharaoh.png', name: '𓉔𓍢𓃭𓄿𓂋𓄿𓍯'),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CharacterCard extends StatelessWidget {
+  const _CharacterCard({required this.imageUrl, required this.name});
+
+  final String imageUrl;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => () {},
+      child: Container(
+        width: double.infinity,
+        height: 170,
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            image: AssetImage(imageUrl),
+            fit: BoxFit.cover,
+            colorFilter: const ColorFilter.mode(Colors.black26, BlendMode.srcATop), //
+          ),
+        ),
+        child: Text(name,
+            style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 22)),
       ),
     );
   }
