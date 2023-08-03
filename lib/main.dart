e0620841dd86720be974b4712269001ae014d964
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-08-03 20:13:45 +0900

    [fix]iOSのpush通知設定

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:runpush/service/api_service.dart';
+import 'package:runpush/service/push_notification_service.dart';
 import 'package:runpush/ui/view/top_view.dart';
 
 import 'firebase_options.dart';
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    PushNotificationService().initialize();
     return GetMaterialApp(
       title: 'RUNPUSH',
       theme: ThemeData(),
